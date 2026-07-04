cccb542b564bf009624da1da5ee77d2d4b767dad
Rahul <9905singhrahul@gmail.com>
2026-07-04 23:17:55 +0530

    Studied the complete flow of RAG.

---
+++ 10.Text_Splitter/1_length_based.py
@@ -0,0 +1,22 @@
+from langchain_community.document_loaders import PyPDFLoader
+
+try:
+    from langchain_text_splitters import CharacterTextSplitter
+except ImportError:
+    from langchain.text_splitter import CharacterTextSplitter
+
+loader = PyPDFLoader('dl-curriculum.pdf')
+
+docs = loader.load()
+
+splitter = CharacterTextSplitter(
+    chunk_size=100,
+    chunk_overlap=10,
+    separator=''
+)
+
+texts = splitter.split_documents(docs)
+
+print(f'Number of chunks: {len(texts)}')
+print(texts[0].page_content)
+print(texts[1].page_content)
